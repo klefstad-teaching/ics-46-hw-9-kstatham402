ac01c8233b13680b6079260ab19b2283416ca408
kstatham402 <klstatham222@gmail.com>
2025-03-08 21:05:35 -0800

    Change PRINT

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -112,7 +112,7 @@ void load_words(set<string> & word_list, const string& file_name) {
     file.close();
 }
 void print_word_ladder(const vector<string>& ladder) {
-    if (ladder.size() == 0) cout << "No ladder found." << endl;
+    if (ladder.size() == 0) cout << "No word ladder found." << endl;
     else {
         cout << "Word ladder found: ";
         for(const auto &word : ladder) {
