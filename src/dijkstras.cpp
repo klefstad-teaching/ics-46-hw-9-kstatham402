43bbb9432c9af9cef30f1218395939d614f0e709
kstatham402 <klstatham222@gmail.com>
2025-03-09 01:19:00 -0800

    Add debug to extract

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -51,11 +51,14 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous,
     int destination) {
     vector<int> shortest_path;
+    cout << "\n\nShortest path from source: " << destination << endl;
     if (distances[destination] == INF) {
+        cout << "No path found from source: " << destination << endl;
         return shortest_path;
     }
     for (int i = destination; i != -1; i = previous[i]) {
         shortest_path.push_back(i);
+        cout << "Pushing back node " << i << endl;
     }
     reverse(shortest_path.begin(), shortest_path.end());
     return shortest_path;
