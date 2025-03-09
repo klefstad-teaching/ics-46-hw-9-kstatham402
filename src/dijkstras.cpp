ebaf77faa579465fc3cba3c2266747ef8f4af77d
kstatham402 <klstatham222@gmail.com>
2025-03-09 01:24:04 -0800

    Adding one one cout

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -16,27 +16,27 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
     pq.push(Edge(source, 0));
     distance[source] = 0;
-    cout << "Initial distances: ";
-    for (int i = 0; i < n; ++i) {
-        cout << distance[i] << " ";
-    }
-    cout << endl;
+    // cout << "Initial distances: ";
+    // for (int i = 0; i < n; ++i) {
+    //     cout << distance[i] << " ";
+    // }
+    // cout << endl;
     while (!pq.empty()) {
         Edge current = pq.top();
         pq.pop();
         int u = current.src;
-        cout << "Processing node: " << u << " with distance: " << current.weight << endl;
+        // cout << "Processing node: " << u << " with distance: " << current.weight << endl;
         if (visited[u]) {continue;}
         visited[u] = true;
-        cout << "Marking node " << u << " as visited" << endl;
+        // cout << "Marking node " << u << " as visited" << endl;
         for (const Edge& neighbor : G[u]) {
             int v = neighbor.dst;
             int weight = neighbor.weight;
-            cout << "Checking neighbor: " << v << " with edge weight: " << weight << endl;
+            // cout << "Checking neighbor: " << v << " with edge weight: " << weight << endl;
             if (!visited[v] && distance[u]+weight < distance[v]) {
                 distance[v] = distance[u]+weight;
                 previous[v] = u;
-                cout << "Updating distance for node " << v << " to " << distance[v] << endl;
+                // cout << "Updating distance for node " << v << " to " << distance[v] << endl;
                 pq.push(Edge(v, distance[v]));
             }
         }
@@ -56,10 +56,10 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
         // cout << "No path found from source: " << destination << endl;
         return shortest_path;
     }
-    // for (int i = destination; i != -1; i = previous[i]) {
-    //     shortest_path.push_back(i);
-    //     cout << "Pushing back node " << i << endl;
-    // }
+    for (int i = destination; i != -1; i = previous[i]) {
+        shortest_path.push_back(i);
+        cout << "Pushing back node " << i << endl;
+    }
     reverse(shortest_path.begin(), shortest_path.end());
     return shortest_path;
 }
